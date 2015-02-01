2dd5abbacabc3cc28e5786abb8698da3e4eaf277
Zhaoyu Wei <weixiaobo88@gmail.com>
2015-02-01 21:25:39 +0800

    zy: finish section_2_practice_1

--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,8 +1,26 @@
 function count_same_elements(collection)
 {
     //在这里写入代码
+    var result = [];
+    var key = [];
+    var count = [];
+
+    collection.forEach(function(each) {
+        var index = key.indexOf(each);
+        if(index != -1) {
+            count[index]++;
+        }else {
+            key.push(each);
+            count.push(1);
+        }
+    });
+
+    key.forEach(function(each, index) {
+       result.push({key: each, count: count[index]});
+    });
+
+    return result;
 }
 
 
 module.exports = count_same_elements;
-
