2070513ee98d1a6c121b9017962a0be97b282708
Zhaoyu Wei <weixiaobo88@gmail.com>
2015-02-01 12:15:49 +0800

    zy: finish section_1_practice2

--- practices/section_1/practice_2/practice.js
+++ practices/section_1/practice_2/practice.js
@@ -1,6 +1,20 @@
 function collect_same_elements(collection_a, collection_b)
 {
     //在这里写入代码
+    var result = [];
+    var index = 0;
+
+    collection_a.forEach(function(each_in_a) {
+        collection_b.forEach(function(each_in_b) {
+            each_in_b.forEach(function(each_item) {
+                if(each_in_a === each_item) {
+                    result.push(each_in_a);
+                }
+            })
+        })
+    });
+
+    return result;
 }
 
 module.exports = collect_same_elements;
