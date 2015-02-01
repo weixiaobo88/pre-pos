fcf18e6c7b60eb5e43662e05ad72ad9c4092a7a1
Zhaoyu Wei <weixiaobo88@gmail.com>
2015-02-01 12:03:30 +0800

    zy: finish the section1_practice_1 implemetation

--- practices/section_1/practice_1/practice.js
+++ practices/section_1/practice_1/practice.js
@@ -1,8 +1,18 @@
 function collect_same_elements(collection_a, collection_b)
 {
     //在这里写入代码
+    var result = [];
+
+    collection_a.forEach(function(each_in_a) {
+        collection_b.forEach(function(each_in_b) {
+            if(each_in_a === each_in_b) {
+                result.push(each_in_a);
+            }
+        })
+    });
+
+    return result;
 }
 
 module.exports = collect_same_elements;
-
 
