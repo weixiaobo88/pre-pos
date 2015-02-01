c72f17decb5c8c8549f0f0c7a1394609ccbe9eff
Zhaoyu Wei <weixiaobo88@gmail.com>
2015-02-01 12:19:40 +0800

    zy: finish section_1_practice_3

--- practices/section_1/practice_3/practice.js
+++ practices/section_1/practice_3/practice.js
@@ -1,6 +1,18 @@
 function collect_same_elements(collection_a, object_b) {
     //在这里写入代码
 
+    var collection_b = object_b.value;
+    var result = [];
+
+    collection_a.forEach(function(each_in_a) {
+        collection_b.forEach(function(each_in_b) {
+            if(each_in_a === each_in_b) {
+                result.push(each_in_a);
+            }
+        })
+    });
+
+    return result;
 }
 
 module.exports = collect_same_elements;
--- specs/section_1/practice_3_spec.js
+++ specs/section_1/practice_3_spec.js
@@ -1,6 +1,6 @@
 var collect_same_elements = require("../../practices/section_1/practice_3/practice.js");
 
-xdescribe('practice-1-3', function () {
+describe('practice-1-3', function () {
 
     var collection_a = ["a", "e", "h", "t", "f", "c", "g", "b", "d"];
     var collection_b = {value: ["a", "d", "e", "f"]};
@@ -8,7 +8,7 @@ xdescribe('practice-1-3', function () {
     it("选出A集合中跟B对象中value属性中的元素相同的元素", function () {
         var result = collect_same_elements(collection_a, collection_b);
 
-        expect(result).toBe(["a", "e", "f", "d"]);
+        expect(result).toEqual(["a", "e", "f", "d"]);
     });
 
 
