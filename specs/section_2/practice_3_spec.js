c73a04c8396eb5fc773613fed269620cd60d827f
Zhaoyu Wei <weixiaobo88@gmail.com>
2015-02-01 11:55:51 +0800

    zy: ignore test cases not implemented

--- specs/section_2/practice_3_spec.js
+++ specs/section_2/practice_3_spec.js
@@ -1,6 +1,6 @@
 var count_same_elements = require("../../practices/section_2/practice_3/practice.js");
 
-describe('practice-1-3', function () {
+xdescribe('practice-1-3', function () {
 
     var collection_a = [
             "a","a","a",
