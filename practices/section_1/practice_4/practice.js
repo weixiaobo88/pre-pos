28008680427d26bd96da39faf14cc1ba9e560307
Zhaoyu Wei <weixiaobo88@gmail.com>
2015-02-01 12:23:43 +0800

    zy: finish section_1_practice_4

--- practices/section_1/practice_4/practice.js
+++ practices/section_1/practice_4/practice.js
@@ -1,6 +1,23 @@
 function collect_same_elements(collection_a, object_b) {
     //在这里写入代码
 
+    var key_in_a = [];
+    var value_in_b = object_b.value;
+    var result = [];
+
+    collection_a.forEach(function(each_in_a) {
+        key_in_a.push(each_in_a.key);
+    });
+
+    key_in_a.forEach(function(each_key) {
+        value_in_b.forEach(function(each_value) {
+            if(each_key === each_value) {
+                result.push(each_key);
+            }
+        })
+    });
+
+    return result;
 }
 
 module.exports = collect_same_elements;
--- specs/section_1/practice_4_spec.js
+++ specs/section_1/practice_4_spec.js
@@ -1,6 +1,6 @@
 var collect_same_elements = require("../../practices/section_1/practice_4/practice.js");
 
-xdescribe('practice-1-4', function () {
+describe('practice-1-4', function () {
 
     var collection_a = [{key: "a"}, {key: "e"}, {key: "h"}, {key: "t"}, {key: "f"}, {key: "c"}, {key: "g"}, {key: "b"}, {key: "d"}];
     var collection_b = {value: ["a", "d", "e", "f"]};
@@ -8,7 +8,7 @@ xdescribe('practice-1-4', function () {
     it("选出A集合中元素的key属性，跟B对象中value属性中的元素相同的元素", function () {
         var result = collect_same_elements(collection_a, collection_b);
 
-        expect(result).toBe(["a", "e", "f", "d"]);
+        expect(result).toEqual(["a", "e", "f", "d"]);
     });
 
 
